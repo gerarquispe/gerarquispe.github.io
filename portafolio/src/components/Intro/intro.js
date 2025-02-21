80880b3b4f905bab8945b8621e351f7180fe95f6
Gerar Quispe <fryzito@gmail.com>
2025-02-21 04:05:10 -0500

    Portafolio(Intro):fixing ref of button Hire Me.Desk-20250221.

--- portafolio/src/components/Intro/intro.js
+++ portafolio/src/components/Intro/intro.js
@@ -2,7 +2,6 @@ import React from "react";
 import './intro.css';
 import bg from '../../assets/profil-gerar-photo-v4.png';
 import btnImg from '../../assets/hireme.png';
-import { Link } from 'react-scroll';
 
 import ParticlesComponent from "../particles";
 
@@ -15,14 +14,14 @@ const Intro = () => {
                 <span className="introText">I'm <span className="introName"> Gerar Francis </span> <br /> Master in Computer Science </span>
                 <p className="introPara">I am a skilled web page builder with experience in creating <br />visually appealing and user friendly websites.</p>
                 <p className="introPara">In addition to that, I work with topics related to Machine Learning, large-scale data processing and computer vision.</p>
-                <button className="btn">
-                        <img src={btnImg} alt="Hire" className='btnImg' />
-                        Hire Me
-                </button>
+                <a href="https://www.linkedin.com/in/gfquispe/" target="_blank" rel="noopener noreferrer">
+                    <button className="btn" >
+                            <img src={btnImg} alt="Hire" className='btnImg' />
+                            Hire Me
+                    </button>
+                </a>
             </div>
-
-            <img src={bg} alt="Profile" className="bg" />
-            
+            <img src={bg} alt="Profile" className="backgroundImg" />
         </section>
     )
 }
