baa345bc5cee14d51a4ea74c3115da75c3ea4e6a
Gerar Quispe <fryzito@gmail.com>
2025-07-23 17:27:13 -0500

    fix(contact):del form and adding two more icons.desk20250723

--- portafolio/src/components/Contact/contact.js
+++ portafolio/src/components/Contact/contact.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { Mail, MessageSquareText } from 'lucide-react';
 import './contact.css';
 import Walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
@@ -47,32 +48,49 @@ const Contact = () => {
                 <img src={Facebook} alt="" className="clientImg" />
             </div>
         </div>
+
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
-            <span className="contactDesc">Please fill out the form below to discuss any word opportunityties.</span>
-            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <span className="contactDesc">Feel free to contact with me if you have any questions:</span>
+
+
+            <div className="links-icon">
+              <a 
+              href="https://api.whatsapp.com/send?phone=51951128230&text=Hi%20Francis!%20I%20saw%20your%20website%20and%20I%20am%20interested%20in%20hiring%20your%20services."
+              className="" target="_blank" rel="noopener noreferrer"
+              >
+                <MessageSquareText className="link-icon" />
+              </a>
+
+              <a href="mailto:gerarfrancisq@gmail.com" className="">
+                <Mail className="link-icon" />
+              </a>
+            </div>
+
+
+            {/* <form className="contactForm" ref={form} onSubmit={sendEmail}>
                 <input type="text" className="name" placeholder="Your Name" name='your_name'/>
                 <input type="email" className="email" placeholder="Your Email" name='your_email'/>
                 <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type='submit' value='Send' className="submitBtn">Submit</button>
+            </form> */}
 
-                <div className="links">
-                  <a href="https://www.facebook.com/gfquispe" target="_blank" rel="noopener noreferrer">
-                      <img src={FacebookIcon} alt="Facebook" className="link" />
-                  </a>
-                  <a href="https://x.com/GerarQuispe" target="_blank" rel="noopener noreferrer">
-                      <img src={TwiterIcon} alt="Twitter" className="link" />
-                  </a>
-                  <a href="https://www.youtube.com/@gfquispe" target="_blank" rel="noopener noreferrer">
-                      <img src={YouTubeIcon} alt="Youtube" className="link" />
-                  </a>
-                  <a href="https://www.instagram.com/gerar.francis/" target="_blank" rel="noopener noreferrer">
-                      <img src={InstagramIcon} alt="Instagram" className="link" />
-                  </a>
-                </div>
-
-            </form>
+            <div className="links">
+              <a href="https://www.facebook.com/gfquispe" target="_blank" rel="noopener noreferrer">
+                  <img src={FacebookIcon} alt="Facebook" className="link" />
+              </a>
+              <a href="https://x.com/GerarQuispe" target="_blank" rel="noopener noreferrer">
+                  <img src={TwiterIcon} alt="Twitter" className="link" />
+              </a>
+              <a href="https://www.youtube.com/@gfquispe" target="_blank" rel="noopener noreferrer">
+                  <img src={YouTubeIcon} alt="Youtube" className="link" />
+              </a>
+              <a href="https://www.instagram.com/gerar.francis/" target="_blank" rel="noopener noreferrer">
+                  <img src={InstagramIcon} alt="Instagram" className="link" />
+              </a>
+            </div>
         </div>
+
     </section>
   );
 }
